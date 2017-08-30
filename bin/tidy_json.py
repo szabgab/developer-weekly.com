59b8993bc2aca422ebda464de89b913e1d956ad2
Gabor Szabo <gabor@szabgab.com>
2017-08-30 18:54:15 +0300

    code to tidy the json files

---
+++ bin/tidy_json.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import json
+import glob
+
+# format the json files
+
+def tidy(filename):
+    try:
+        with open(filename) as fh:
+            data = json.load(fh)
+        with open(filename, 'w') as fh:
+            json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
+    except Exception as e:
+        print('Exception while processing {}'.format(filename))
+        print(e)
+
+for filename in glob.glob("src/*.json"):
+    if filename == 'src/next.json':
+        continue
+    tidy(filename)
+
+# vim: expandtab
+
